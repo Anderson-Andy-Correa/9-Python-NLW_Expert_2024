b25618df5aa9eaf5863cf8d05e8bf2f58d3e92f0
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-11 23:02:31 -0300

    feat: Implementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,17 @@
 from src.views.http_types.http_reposnse import HttpReponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntutyError
 
 def handle_errors(error: Exception) -> HttpReponse:
+    if isinstance(error, HttpUnprocessableEntutyError):
+        return HttpReponse(
+            status_code = error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
     return HttpReponse(
         status_code=500,
         body={
